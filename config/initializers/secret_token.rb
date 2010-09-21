c545067b219760fdcd2b03be122b8d2a2a70f129
Louis-Alban KIM <louis.alban.kim@virtuality2reality.com>
2010-09-21 11:25:44 +0200

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tracedeson::Application.config.secret_token = 'c08516e1ef5bbc5832ef0bb9d536b26d958130e0d0cf19007e6f9c067f5fc35a8311a3cf06cf151bf40cbc818872f6701793e8e9f5875ef7bcd27079d3d2c3c7'
